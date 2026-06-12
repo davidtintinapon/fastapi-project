10786228c120bff37d0ecf44da42c827ed239be9
David Tintinapon <davidtintinapon.dev@gmail.com>
2026-06-11 20:19:33 -0500

    day 17: multiple endpoints

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/people")
+def people():
+    return {"people":[
+        {"name": "Juan"},
+        {"name": "Marco"}
+    ]}
+
+@app.get("/study")
+def course():
+    return {"course": "FastAPI", "level": "Beginner"}
