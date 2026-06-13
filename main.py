6887d7963d1e9ab267d75d05120a7003bb2bf3ef
David Tintinapon <davidtintinapon.dev@gmail.com>
2026-06-13 17:23:52 -0500

    day 19: POST endpoint with Pydantic model

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Student(BaseModel):
+    name: str
+    age: int
+    career: str
 
 @app.get("/")
 def read_root():
@@ -20,3 +26,7 @@ def course():
 @app.get("/people/{person_id}")
 def get_people(person_id : int):
     return {"person_id": person_id, "message": "Estudiante encontrado"}
+
+@app.post("/students")
+def create_student(student: Student):
+    return {"message": "Estudiante creado", "student": student}
