deb90049753331c338a20ffd54bf04445e51036d
David Tintinapon <davidtintinapon.dev@gmail.com>
2026-06-14 22:31:25 -0500

    students API: create, list and get by id

---
+++ students_api.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Student(BaseModel):
+    name: str
+    age: int
+    career: str
+
+students = []
+
+@app.post("/students")
+def create_student(student: Student):
+    students.append(student)
+    return {"message": "Estudiante creado", "student": student}
+
+@app.get("/students")
+def view_student():
+    return {"students": students}
+
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
+    if 0 <= student_id < len(students):
+        return {"student": students[student_id]}
+    else:
+        return {"error": "Estudiante no encontrado"}
